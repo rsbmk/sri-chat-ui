482b415f1fc0af581ec21692c79fdddcc5ea4322
Roberto Bocio Melo <elpicapica01@gmail.com>
2025-01-20 18:01:37 -0500

    refactor: separate message service

--- src/client/sendMessage.ts
+++ src/client/sendMessage.ts
@@ -1,55 +1,48 @@
-import { createAssistantMessageTemplate, getUserId, userMessageTemplate } from './utils.ts';
-const BASE_URL = import.meta.env.PUBLIC_API_URL
+import { send } from './service.ts';
+import { createAssistantMessageTemplate, userMessageTemplate } from './utils.ts';
+const SCROLL_HEIGHT = 1000
 
+const input = document.getElementById("input") as HTMLInputElement;
+const chat = document.getElementById("chat");
+const loader = document.getElementById("loader") as HTMLDivElement
 
 export async function handleSendMessage(event: SubmitEvent) {
   event.preventDefault();
-
-  const input = document.getElementById("input") as HTMLInputElement;
-  const chat = document.getElementById("chat");
-  const loader = document.getElementById("loader") as HTMLDivElement
-
   if (!input || !chat) return;
 
-  loader.style.display = "flex"
-
+  toogleLoader()
   chat.innerHTML += userMessageTemplate(input.value);
-  const thread_id = await getUserId();
-
   input.disabled = true
 
-  fetch(`${BASE_URL}/chats/input`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ input: input.value, thread_id }),
-  })
-    .then(async (response) => {
-      input.value = "";
-      if (!response.body) {
-        throw new Error("ReadableStream not supported in this environment.");
-      }
+  try {
+    const response = await send(input.value)
+    input.value = "";
+    if (!response.body) {
+      throw new Error("ReadableStream not supported in this environment.");
+    }
 
-      const reader = response.body.getReader();
-      const decoder = new TextDecoder("utf-8");
-      const addChunks = createAssistantMessageTemplate(chat)
+    const reader = response.body.getReader();
+    const decoder = new TextDecoder("utf-8");
+    const addChunks = createAssistantMessageTemplate(chat)
 
-      // Procesa los chunks a medida que llegan
-      while (true) {
-        const { done, value } = await reader.read();
-        if (done) break
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) break
 
-        const chunk = decoder.decode(value, { stream: true });
-        addChunks(chunk);
-      }
+      const chunk = decoder.decode(value, { stream: true });
+      addChunks(chunk);
+      chat.scrollTo(0, chat.scrollHeight + SCROLL_HEIGHT);
+    }
 
-      input.disabled = false
-      input.focus();
-    })
-    .catch((error) => {
-      console.error("Error in streaming fetch:", error);
-    }).finally(() => {
-      loader.style.display = "none"
-    });
+    input.disabled = false
+  } catch (error) {
+    console.error("Error in streaming fetch:", error);
+  } finally {
+    input.focus();
+    toogleLoader()
+  }
+}
+
+function toogleLoader() {
+  loader.style.display = loader.style.display === "flex" ? "none" : "flex"
 }
---
+++ src/client/service.ts
@@ -0,0 +1,22 @@
+import { getUserId } from "./utils";
+
+const BASE_URL = import.meta.env.PUBLIC_API_URL
+
+/**
+ * Sends a chat message to the server.
+ *
+ * @param input - The message content to be sent.
+ * @returns A promise that resolves to the server's response.
+ */
+
+export async function send(input: string) {
+  const thread_id = await getUserId();
+
+  return fetch(`${BASE_URL}/chats/input`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ input, thread_id }),
+  })
+}
